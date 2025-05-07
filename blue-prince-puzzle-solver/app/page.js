667261a630865f334f9ce1935fa3893bef2d5e84
NicDunning <dunningnicolas@gmail.com>
2025-05-07 15:17:48 -0400

    Modularize rules and puzzle tile

--- blue-prince-puzzle-solver/app/page.js
+++ blue-prince-puzzle-solver/app/page.js
@@ -1,4 +1,6 @@
 'use client';
+import PuzzleTile from "./components/puzzle_box/puzzle_tile";
+import RulesBox from "./components/rules_box/rules";
 import styles from "./page.module.css";
 import React, { useState } from 'react';
 
@@ -16,31 +18,6 @@ export default function Home() {
     {color: 8, active: false},
   ])
 
-  const corner = [
-    styles.top_left,
-    styles.normal,
-    styles.top_right,
-    styles.normal,
-    styles.normal,
-    styles.normal,
-    styles.bottom_left,
-    styles.normal,
-    styles.bottom_right
-  ]
-
-  const color_rules = [
-    {color:'White', style_color: styles.white, rules:'Change nearby Gray -> White, if none becomes Gray'},
-    {color:'Gray', style_color: styles.gray, rules:'Nothing'},
-    {color:'Black', style_color: styles.black, rules:'Moves tile in row to the right'},
-    {color:'Red', style_color: styles.red, rules:'Changes White -> Black, Black -> Red'},
-    {color:'Green', style_color: styles.green, rules:'Swaps with tile across'},
-    {color:'Yellow', style_color: styles.yellow, rules:'Swaps with tile above'},
-    {color:'Purple', style_color: styles.purple, rules:'Swaps with tile below'},
-    {color:'Pink', style_color: styles.pink, rules:'Rotates everything around it clockwise'},
-    {color:'Orange', style_color: styles.orange, rules:'Becomes majority adjacent color, nothing if none'},
-    {color:'Blue', style_color: styles.blue, rules:'Copies tile in the middle space'},
-  ]
-
   return (
     <div className={styles.main}>
       <div className={styles.box}>
@@ -48,22 +25,12 @@ export default function Home() {
           {
             grid.map((c, idx)=>{
               return (
-                <div className={`${styles.gridItemBorder} ${corner[idx]}`} key={`interactive-${idx}`}>
-                  <div onClick={() => {console.log(c.color)}} key={`interactive-button-${idx}`} className={`${styles.gridItem} ${color_rules[c.color].style_color} ${corner[idx]}`}></div>
-                </div>
+                <PuzzleTile key={`puzzleTile-${idx}`} c={c} idx={idx}/>
               )
             })
           }
         </div>
-        <div className={styles.rules}>
-          {
-            color_rules.map((c, idx)=>{
-              return(
-                <p className={styles.text} key={`${c.color}-rules`}><em className={c.style_color}>||||</em> : {c.rules}</p>
-              )
-            })
-          }
-        </div>
+        <RulesBox/>
       </div>
       <div className={styles.actionSequence}>
         <div  className={styles.state}>
@@ -72,9 +39,7 @@ export default function Home() {
             {
               grid.map((c, idx)=>{
                 return (
-                  <div className={`${styles.gridItemBorder} ${corner[idx]}`} key={`start-state-${idx}`}>
-                    <div onClick={() => {console.log(c.color)}} key={`start-state-button-${idx}`} className={`${styles.startGridItem} ${color_rules[c.color].style_color} ${corner[idx]}`}></div>
-                  </div>
+                  <PuzzleTile key={`initialState${idx}`} c={c} idx={idx} size={'sm'}/>
                 )
               })
             }
@@ -101,9 +66,7 @@ export default function Home() {
             {
               grid.map((c, idx)=>{
                 return (
-                  <div className={`${styles.gridItemBorder} ${corner[idx]}`} key={`solved-state-${idx}`}>
-                    <div onClick={() => {console.log(c.color)}} key={`solved-state-button-${idx}`} className={`${styles.endGridItem} ${color_rules[c.color].style_color} ${corner[idx]}`}></div>
-                  </div>
+                  <PuzzleTile key={`solvedState${idx}`} c={c} idx={idx} size={'sm'}/>
                 )
               })
             }
