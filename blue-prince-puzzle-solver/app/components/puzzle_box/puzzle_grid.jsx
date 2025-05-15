c8b0e1fbd2b046939cd0446c4ed3d0d53da5f0f1
NicDunning <dunningnicolas@gmail.com>
2025-05-14 22:05:30 -0400

    Fix grid bugs (grid state)

--- blue-prince-puzzle-solver/app/components/puzzle_box/puzzle_grid.jsx
+++ blue-prince-puzzle-solver/app/components/puzzle_box/puzzle_grid.jsx
@@ -3,19 +3,7 @@ import PuzzleTile from "./puzzle_tile";
 import Popup from "../color_pick_popup/color_pick"
 import styles from "./puzzle_grid.module.css";
 
-export default function Grid ({ header }){
-
-    const [grid, setGrid] = useState([
-        {color: 0},
-        {color: 0},
-        {color: 0},
-        {color: 0},
-        {color: 0},
-        {color: 0},
-        {color: 0},
-        {color: 0},
-        {color: 0},
-    ])
+export default function Grid ({ header, grid, setGrid}){
 
     const [isColorPopupOpen, setColorPopupIsOpen] = useState(false)
 
@@ -36,12 +24,15 @@ export default function Grid ({ header }){
                     )
                 })
                 }
-                <Popup isOpen={isColorPopupOpen}
+                <Popup 
+                    key={`${header}-grid-popup`}
+                    isOpen={isColorPopupOpen}
+                    onClose={() => {
+                        setColorPopupIsOpen(false)
+                    }}
                     grid={grid}
                     setGrid={setGrid}
-                    onClose={() => {
-                    setColorPopupIsOpen(false)
-                }}/>
+                />
             </div>
         </div>
     )
